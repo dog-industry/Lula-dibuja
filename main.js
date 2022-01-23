9251cb0eeb8e720b1d6a6c2132feee592566623c
dog-industry <91227995+dog-industry@users.noreply.github.com>
2022-01-23 13:06:34 -0600

    pre 1.0.0
    una versión para disfrutar lo básico con 3 colores y nuevas funciones

--- main.js
+++ main.js
@@ -1,20 +1,47 @@
 var canvas=document.getElementById("mi_canvas");
 var ctx=canvas.getContext("2d");
-var color="purple"
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=2
-ctx.arc(200,200,40,0,2*Math.PI);
-ctx.stroke();
+var color="red"
+var mouseEvent="empty"
+var ultima_posicion_de_x
+var ultima_posicion_de_y
+var ancho_de_linea=1
 canvas.addEventListener("mousedown",mi_mousedown);
 
 function mi_mousedown(l){
-    mouse_x=l.clientX-canvas.offsetLeft;
-    mouse_y=l.clientY-canvas.offsetTop;
-    circle(mouse_x, mouse_y)
+    mouseEvent="mouseDown";
 }
-function circle(mx,my){
-    ctx.beginPath();
-    ctx.arc(mx,my,40,0,2*Math.PI);
-    ctx.stroke();
+canvas.addEventListener("mousemove",mi_mousemove);
+function mi_mousemove(c){
+    posicion_actual_de_mouse_x=c.clientX-canvas.offsetLeft
+    posicion_actual_de_mouse_y=c.clientY-canvas.offsetTop
+    if(mouseEvent=="mouseDown"){
+        ctx.beginPath();
+        ctx.strokeStyle=color;
+        ctx.lineWidth=ancho_de_linea;
+        ctx.moveTo(ultima_posicion_de_x, ultima_posicion_de_y);
+        ctx.lineTo(posicion_actual_de_mouse_x, posicion_actual_de_mouse_y);
+        ctx.stroke();
+    }
+    ultima_posicion_de_x=posicion_actual_de_mouse_x
+    ultima_posicion_de_y=posicion_actual_de_mouse_y
+}
+canvas.addEventListener("mouseup",mi_mouseup);
+function mi_mouseup(l){
+    mouseEvent="mouseUp";
+}
+canvas.addEventListener("mouseleave",mi_mouseleave);
+function mi_mouseleave(l){
+    mouseEvent="mouseLeave";
+}
+function borrar(){
+    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+}
+function rojito(){
+    color="red"
+}
+function azulito(){
+    color="blue"
+}
+function verdesito(){
+    color="green"
 }
